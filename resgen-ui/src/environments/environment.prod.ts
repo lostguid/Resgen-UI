7b5db67a52468cf4acd6e20309728958652543e8
Harshith <lostguid@outlook.com>
2025-03-05 09:58:45 -0500

    updated env

--- resgen-ui/src/environments/environment.prod.ts
+++ resgen-ui/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://staging.example.com/api',
-    auth: {
-      domain: 'harshith-reddy.us.auth0.com',
-      clientId: 'SZyi2Y3UDCmUvP3iYZ2xtrP8MFIJr1xo',
-      audience: 'https://staging.example.com/api',
-      redirectUri: 'http://localhost:4200/home',
-      scope: 'openid profile email'
-    }
-  };
+  production: false,
+  apiUrl: 'https://staging.example.com/api',
+  auth: {
+    domain: 'resgen-ai.us.auth0.com',
+    clientId: 'rsY5nSoucGcdP2wPKOSA45KquQIEEY6T',
+    audience: 'https://staging.example.com/api',
+    redirectUri: 'http://localhost:4200/home',
+    logOutUri: 'http://localhost:4200/home',
+    scope: 'openid profile email'
+  }
+};
--- resgen-ui/src/environments/environment.ts
+++ resgen-ui/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     apiUrl: 'https://staging.example.com/api',
     auth: {
-      domain: 'harshith-reddy.us.auth0.com',
-      clientId: 'SZyi2Y3UDCmUvP3iYZ2xtrP8MFIJr1xo',
+      domain: 'resgen-ai.us.auth0.com',
+      clientId: 'rsY5nSoucGcdP2wPKOSA45KquQIEEY6T',
       audience: 'https://staging.example.com/api',
       redirectUri: 'http://localhost:4200/home',
       logOutUri: 'http://localhost:4200/home',
