4fcf409233dd06a7343f4205d4a3a189eccf7936
Harshith <lostguid@outlook.com>
2025-03-13 11:12:25 -0400

    updated domain name

--- resgen-ui/src/environments/environment.prod.ts
+++ resgen-ui/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
     domain: 'resgen-ai.us.auth0.com',
     clientId: 'vcxOOz5DWFxImTeN2ToQZPojV8YUBqK9',
     audience: 'https://resgen-ai.us.auth0.com/api/v2/',
-    redirectUri: 'https://delightful-desert-0db1b370f.6.azurestaticapps.net/',
-    logOutUri: 'https://delightful-desert-0db1b370f.6.azurestaticapps.net/',
+    redirectUri: 'https://resgen.ai/',
+    logOutUri: 'https://resgen.ai/',
     scope: 'openid profile email'
   }
 };
